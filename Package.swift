f62710b8a748f86b838cf3b0aa6cf09ded0626d8
Kedar Nagekar <kedarnagekar@gmail.com>
2024-07-21 13:39:22 +0530

    - Added required files

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RandomColorPackage",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "RandomColorPackage",
+            targets: ["RandomColorPackage"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kedarnagekar/ColorCreator.git", from: "1.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "RandomColorPackage"),
+        .testTarget(
+            name: "RandomColorPackageTests",
+            dependencies: ["RandomColorPackage"]),
+    ]
+)
